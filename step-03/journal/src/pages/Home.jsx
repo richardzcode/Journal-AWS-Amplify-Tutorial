d3f2de90141161b139ecf51a348ac08a2914181d
Richard Zhang <richardzcode@gmail.com>
2018-09-22 17:27:49 -0700

    undo test code

--- step-03/journal/src/pages/Home.jsx
+++ step-03/journal/src/pages/Home.jsx
@@ -1,16 +1,9 @@
 import React, { Component } from 'react';
 import { Lead, BSpan } from 'bootstrap-4-react';
-import { Auth } from 'aws-amplify';
 
 export default class Home extends Component {
   render() {
     const { user } = this.props;
-
-    if (user) {
-      Auth.userAttributes(user)
-        .then(attributes => console.log('attributes', attributes))
-        .catch(err => console.log('attributes error', err));
-    }
 
     return (
       <React.Fragment>
