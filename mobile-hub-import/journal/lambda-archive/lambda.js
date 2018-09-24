bb0b9cc5987ced57211917f5329c51d0798028c9
Richard Zhang <richardzcode@gmail.com>
2018-09-24 16:28:30 -0700

    remove mobile-hub-import

--- mobile-hub-import/journal/lambda-archive/lambda.js
+++
@@ -1,9 +0,0 @@
-const aws_express = require('aws-serverless-express')
-const app = require('./app')
-
-const server = aws_express.createServer(app)
-
-exports.handler = (event, context) => {
-    console.log("EVENT: " + JSON.stringify(event));
-    aws_express.proxy(server, event, context)
-}
